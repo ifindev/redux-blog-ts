87047dcec44eac367f8a9fd81737da379d4bd5a7
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-18 16:50:33 +0700

    Add clap data to post state and clap button to post UI

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 const PostsList = () => {
   const posts = useSelector((state) => state.posts)
+
+  const dispatch = useDispatch()
 
   const renderedPosts = posts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '50px',
+          padding: '5px',
+        }}
+      >
+        <button
+          style={{
+            backgroundColor: '#fff',
+            border: 'none',
+            padding: '0px',
+            margin: '0px',
+            fontSize: '20px',
+          }}
+        >
+          👏{' '}
+        </button>
+
+        <span style={{ marginTop: '1px', marginLeft: '5px' }}>{post.clap}</span>
+      </div>
     </article>
   ))
 
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post!', content: 'More text' },
+  { id: '1', title: 'First Post!', content: 'Hello!', clap: 0 },
+  { id: '2', title: 'Second Post!', content: 'More text', clap: 0 },
 ]
 
 const postsSlice = createSlice({
