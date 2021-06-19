8a46788c998d6d024328f7d6c93059e9069899f4
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-20 00:46:17 +0700

    export RootState of store for typescript

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,8 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from './posts/postsSlice'
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     posts: postsReducer,
   },
 })
+
+export default store
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
