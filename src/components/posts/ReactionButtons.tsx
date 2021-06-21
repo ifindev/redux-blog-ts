92d2f5611d73ab27de5db1f848b023cc66c6a320
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-21 19:31:42 +0700

    Added dispatch reaction counter on ReactionButton component

--- src/components/posts/ReactionButtons.tsx
+++ src/components/posts/ReactionButtons.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useAppDispatch } from '../../redux/hooks'
+import { reactionAdded } from '../../redux/posts/postsSlice'
 import PostsState from '../../types/posts'
 
 const reactionEmoji: { [emoji: string]: string } = {
@@ -10,9 +12,18 @@ const reactionEmoji: { [emoji: string]: string } = {
 }
 
 const ReactionButtons: React.FC<{ post: PostsState }> = ({ post }) => {
+  const dispatch = useAppDispatch()
+
   const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
-      <button key={name} type="button" className="muted-button reaction-button">
+      <button
+        key={name}
+        type="button"
+        className="muted-button reaction-button"
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
         {emoji} {post.reactions[name]}
       </button>
     )
